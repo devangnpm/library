3d2e160ada4d2ab4a62e911f843722a1153e239b
devangnpm <d3vang2002@gmail.com>
2024-05-03 23:39:13 +0530

    add new book to card onclick

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const newBookDialog = document.getElementById('newBookDialog');
 const addNewBookButton = document.getElementById('addNewBook');
 const dialogCloseBtn = document.getElementById('close');
+const dialogAddBtn = document.getElementById('btn-Add');
+const myLibrary =[]; 
 
-
-
-/*
-const myLibrary =[];
-
-
+// this is Book Constructor func//
 function Book(title,author,pages,isRead) {
     this.title = title;
     this.author = author;
@@ -20,6 +17,7 @@ function Book(title,author,pages,isRead) {
     }
 }
 
+// this is initial book or test book creation using Obj constructor and pushing into the myLib arr//
 const theHobbit = new Book('The Hobbit','J.R.R Tolkien','295','not read');
 myLibrary.push(theHobbit);
 console.log(theHobbit);
@@ -27,7 +25,7 @@ console.log(theHobbit);
 console.log(theHobbit.info());
 
 
-function addBookToLibrary() {
+dialogAddBtn.addEventListener('click', function addBookToLibrary() {  // function takes inputs and creates a new book Object //
     let getBookTitle = prompt("Title");
     let getBookAuthor = prompt("Author");
     let getBookPages = prompt ("Pages");
@@ -37,16 +35,17 @@ function addBookToLibrary() {
     const bookObject = new Book(getBookTitle,getBookAuthor,getBookPages,getBookRead);
     myLibrary.push(bookObject);
 
-    return bookObject;
-}
+    displayBookOnCard([bookObject]);
+});
 
-const myBook = addBookToLibrary();
+/*const myBook = addBookToLibrary();   
 console.log('First Book',myLibrary[0]);
 console.log('Second Book',myLibrary[1]);
 */
 
 
 addNewBookButton.addEventListener('click', () => {          //Event handling for add new book button//
+    console.log('button clicked');
     newBookDialog.showModal();
     newBookDialog.classList.add('open');
     
@@ -56,3 +55,49 @@ dialogCloseBtn.addEventListener('click', () => {
     newBookDialog.close();
     newBookDialog.classList.remove('open');
 });
+
+displayBookOnCard(myLibrary);
+
+function displayBookOnCard(books) {
+    books.forEach(book => {
+        const newBook = document.createElement('div');
+        newBook.classList.add('book'); 
+
+        const titleParagraph = document.createElement('p');
+        titleParagraph.textContent = book.title;
+        titleParagraph.classList.add('header-p');
+
+        const authorParagraph = document.createElement('p');
+        authorParagraph.textContent = book.author;
+
+        const pagesParagraph = document.createElement('p');
+        pagesParagraph.textContent = book.pages;
+
+         const sliderLabel = document.createElement('label');
+        sliderLabel.classList.add('switch');
+        const sliderInput = document.createElement('input');
+        sliderInput.setAttribute('type', 'checkbox');
+        const sliderSpan = document.createElement('span');
+        sliderSpan.classList.add('slider', 'round');
+        sliderLabel.appendChild(sliderInput);
+        sliderLabel.appendChild(sliderSpan);
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.setAttribute('id', 'remove');
+
+        
+        newBook.appendChild(titleParagraph);
+        newBook.appendChild(authorParagraph);
+        newBook.appendChild(pagesParagraph);
+        newBook.appendChild(sliderLabel);
+        newBook.appendChild(removeButton);
+
+        const booksContainer = document.querySelector('.books');
+        booksContainer.appendChild(newBook);
+
+    });
+}
+
+console.log(myLibrary);
+
